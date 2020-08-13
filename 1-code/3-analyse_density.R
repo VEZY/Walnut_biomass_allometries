c51ec8c0a722f4ce109eaeff5e36bac9ec7ff45f
dauzat <dauzat@cirad.fr>
2020-08-13 18:03:28 +0200

    Update 3-analyse_density.R

--- 1-code/3-analyse_density.R
+++ 1-code/3-analyse_density.R
@@ -12,30 +12,60 @@ df_mtg = fread("2-results/density/density_data.csv", data.table = FALSE)
 df_mtg%>%
   dplyr::filter(ID != 1500 & ID != 1515 & ID != 4296)%>%
   ggplot(aes(x= topological_order, y= density))+
-  geom_point(aes(group = topological_order))
+  geom_boxplot(aes(group = topological_order))
 
 df_mtg%>%
   dplyr::filter(ID != 1500 & ID != 1515 & ID != 4296)%>%
   ggplot(aes(x= segment_index_on_axis, y= density))+
-  geom_point(aes(color = topological_order))
+  geom_point(aes(color = branch))
 
-mtg_df%>%
+df_mtg%>%
+  dplyr::filter(ID != 1500 & ID != 1515 & ID != 4296)%>%
+  ggplot(aes(x= segment_index_on_axis, y= density))+
+  facet_wrap(.~branch)+
+  geom_point(aes(color = branch))
+
+df_mtg%>%
   dplyr::filter(ID != 1500 & ID != 1515)%>%
-  ggplot(aes(x= diameter, y= density_ph))+
-  geom_point(aes(color = topological_order))
-
-mtg_df$ID[mtg_df$density>1 & !is.na(mtg_df$density)]
-
-mtg_df$dry_weight_p1[mtg_df$density>1 & !is.na(mtg_df$density)]
-mtg_df$volume_bh[mtg_df$density>1 & !is.na(mtg_df$density)]
-
-plot(mtg_df$dry_weight_p1 / mtg_df$volume_bh)
-
-autoplot(tree1l)
-plotly_mtg(tree1l)
+  ggplot(aes(x= diameter, y= density))+
+  facet_wrap(.~branch, scales = "free")+
+  geom_point(aes(color = segment_index_on_axis))
 
 
-get_children_values(attribute = "length",node = tree1l$MTG$node_2, symbol = "S",recursive = TRUE)
-# aggregate_children_values(attribute = "length",node = tree1l$MTG$node_2, symbol = "S",recursive = TRUE)
+df_mtg%>%
+  dplyr::filter(ID != 1500 & ID != 1515)%>%
+  ggplot(aes(x= volume, y= density))+
+  facet_wrap(.~branch, scales = "free")+
+  geom_point(aes(color = segment_index_on_axis))
 
-get_follow_values(attribute = "length", node = FindNode(MTG$MTG,"node_8"), symbol = "S")
+df_mtg%>%
+  dplyr::filter(ID != 1500 & ID != 1515)%>%
+  ggplot(aes(x= volume_subtree, y= density))+
+  facet_wrap(.~branch, scales = "free")+
+  geom_point(aes(color = segment_index_on_axis))
+
+df_mtg%>%
+  dplyr::filter(ID != 1500 & ID != 1515)%>%
+  ggplot(aes(x= volume_ph, y= density))+
+  facet_wrap(.~branch, scales = "free")+
+  geom_point(aes(color = segment_index_on_axis))
+
+df_mtg%>%
+  dplyr::filter(density < 1.0)%>%
+  ggplot(aes(x= density, y= density_ph))+
+  geom_abline(slope = 1,intercept = 0)+
+  # facet_wrap(.~branch, scales = "free")+
+  geom_point(aes(color = volume))+
+  scale_color_viridis_c()
+
+
+df_mtg%>%
+  dplyr::filter(density < 1.0)%>%
+  ggplot(aes(x= diameter, y= density_ph/density))+
+  geom_abline(slope = 1,intercept = 0)+
+  # facet_wrap(.~branch, scales = "free")+
+  geom_point(aes(color = volume))+
+  scale_color_viridis_c()
+
+# autoplot(tree1l)
+# plotly_mtg(tree1l)
